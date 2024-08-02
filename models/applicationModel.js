7ad3ba5a76d786e7110fb473a10c9e156ec7a179
Tom Gagliano <tgaglianoll@gmail.com>
2024-08-02 19:01:23 -0400

    add initial properties for application model file

---
+++ models/applicationModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const applicationSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  companyName: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  applicationLink: {
+    type: String,
+    required: true,
+  },
+  remote: {
+    type: Boolean,
+    required: true,
+  },
+  response: {
+    type: String,
+    required: false,
+  },
+  interview: {
+    date: { type: Date },
+    time: { type: String },
+    location: { type: String },
+  },
+  notes: {
+    type: String,
+    required: false, // Made optional for flexibility
+  },
+});
+
+const Application = mongoose.model("Application", applicationSchema);
+
+module.exports = Application;
