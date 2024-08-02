a77898a9cd3730ce42941a04cc52d32c8904c9ad
Tom Gagliano <tgaglianoll@gmail.com>
2024-08-01 22:03:15 -0400

    add mongoose package and initial properties for userModel file

---
+++ models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
