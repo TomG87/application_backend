0dcd6459b575e3a8d7b69f405c871aa2b2959c36
Tom Gagliano <tgaglianoll@gmail.com>
2024-08-09 20:08:25 -0400

    add controller register and login exports

---
+++ controllers/userController.js
@@ -0,0 +1,42 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+try {
+  const existingUser = await User.findOne({ email })
+  if (existingUser) {
+    return.status(400).json({ message: 'User already exists' });
+  }
+
+  const newUser = new User({ name, email, password });
+  await newUser.save();
+
+  res.status(201).json({message: 'User registered successfully'});
+  } catch (err) {
+  res.status(500).json({ message: err.message })
+  };
+
+};
+
+exports.login = async (req, res ) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(400).json({ message: 'Invalid email or password' });
+    }
+    const token = jwt.sign({ id: user_id }, 'your_jwt_secret', { expiresIn: '1 hour' });
+
+    res.json({ token, userId: user._id});
+} catch(err) {
+  res.status(500).json({ message: err.message })
+ }
+};
