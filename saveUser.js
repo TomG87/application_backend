08be246f598e91ee19f5dd1962c8070ed93a171d
Tom Gagliano <tgaglianoll@gmail.com>
2024-08-22 10:24:35 -0400

    Add saveUser.js file for template to be used in REPL to add user

---
+++ saveUser.js
@@ -0,0 +1,21 @@
+// templat for code to be add user in REPL
+
+async function saveUser() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+
+    const user = new User({
+      name: "John Doe",
+      email: "john@example.com",
+      password: "password123",
+    });
+
+    await user.save();
+    console.log("User saved successfully");
+  } catch (err) {
+    console.log("Error saving user:", err);
+  }
+}
+
+saveUser();
