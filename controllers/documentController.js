0c2ddcd2837d5e0cfe28054bf33566031d44e6e2
Tom Gagliano <tgaglianoll@gmail.com>
2024-09-03 21:10:51 -0400

    add multer configuration and imports for document handling in documentController

---
+++ controllers/documentController.js
@@ -0,0 +1,15 @@
+const Document = require("../models/documentModel");
+const User = require("../models/userModel");
+const Application = require("../models/applicationModel");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
