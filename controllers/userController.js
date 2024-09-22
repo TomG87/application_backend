9bb83e205b92cb79deb51083fdce5ff058599cc0
Tom Gagliano <tgaglianoll@gmail.com>
2024-09-21 21:06:55 -0400

    add bcrypt for password hashing and implement hashed password storage

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,7 +12,9 @@ exports.register = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    const newUser = new User({ name, email, password });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ name, email, password: hashedPassword });
     await newUser.save();
 
     res.status(201).json({ message: "User registered successfully" });
@@ -22,26 +24,27 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-  console.log("Login Request Body:", req.body);
   const { email, password } = req.body;
 
   try {
+    console.log("Login Request Body:", req.body);
     const user = await User.findOne({ email });
     if (!user) {
+      console.log("No user found with this email.");
       return res.status(400).json({ message: "Invalid email or password" });
     }
-
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
+      console.log("Password does not match.");
       return res.status(400).json({ message: "Invalid email or password" });
     }
-
     const token = jwt.sign({ id: user._id }, "your_jwt_secret", {
       expiresIn: "1 hour",
     });
 
     res.json({ token, userId: user._id });
   } catch (err) {
+    console.log("Error during login:", err);
     res.status(500).json({ message: err.message });
   }
 };
