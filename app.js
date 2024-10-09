f034e26fbc1a63425d2b08bb3472f10671a8956a
Tom Gagliano <tgaglianoll@gmail.com>
2024-10-09 18:27:43 -0400

    add put route for updating applications in applicationRoute

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoute");
 const applicationRoutes = require("./routes/applicationRoute");
 const cors = require("cors");
+const Application = require("./models/applicationModel");
 
 app.use(
   cors({
@@ -26,7 +27,19 @@ mongoConnect();
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
-app.use("/api/applications", applicationRoutes); // Use application routes here
+app.use("/api/applications", applicationRoutes);
+app.get("/api/applications/:id", async (req, res) => {
+  try {
+    const application = await Application.findById(req.params.id);
+    if (!application) {
+      return res.status(404).send("Application not found");
+    }
+    res.send(application);
+  } catch (error) {
+    console.error("Error fetching application:", error); // Log the full error object
+    res.status(500).send("Server error");
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Testing");
